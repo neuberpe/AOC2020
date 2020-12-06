a4711926f7b9a51984c172ba98920eb654baa317
neuberpe <36083955+neuberpe@users.noreply.github.com>
2020-12-06 14:45:07 +0100

    Update AOC6_20.py

--- AOC6_20.py
+++ AOC6_20.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
 
-with open('AOC6.txt', 'r') as file:
-    data = file.read().replace('\n\n', ',')
+with open("AOC6.txt", "r") as file:
+    data = file.read().replace("\n\n", ",")
 data = data.replace("\n","")
 mylist = data.split(",")
 
 res=[]
 
 for k in mylist:
-	k = list(dict.fromkeys(k))
-	res.append(len(k))
-
+	res.append(len(list(dict.fromkeys(k))))
 
 print(sum(res))
