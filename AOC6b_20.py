989058a6a039bd06f354623efa4a1c954f7356d7
neuberpe <36083955+neuberpe@users.noreply.github.com>
2020-12-06 14:45:29 +0100

    Update AOC6b_20.py

--- AOC6b_20.py
+++ AOC6b_20.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python
 
-with open('AOC6.txt', 'r') as file:
-    data = file.read().replace('\n\n', ',')
+with open("AOC6.txt", "r") as file:
+    data = file.read().replace("\n\n", ",")
 mylist = data.split(",")
-abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 res = 0
 
-test =["abc","a\nb\nc","ab\nac","a\na\na\na","b"]
-
-for k in test:
+for k in mylist:
 	s =""
 	k = k.split("\n")
-	a = len(k)
 	s = list(s.join(k))
-	#s = list(s)
 	for j in abc:
-		if s.count(j) == a:
+		if s.count(j) == len(k):
 			res += 1
 
 print(res)
